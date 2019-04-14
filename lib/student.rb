6b3b8ed21d3865d08438fb2f54118b592f517fb9
Ryan Pethel <arpethel@gmail.com>
2019-04-14 12:02:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :student
 
   def hello
-    puts "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
